917dccc618dee63ae10d598daf9a1fad0571a67d
NeTBoyBot <toolumini@mail.ru>
2023-08-25 21:58:06 +0300

    Add file comments

--- UpBoard.Application.AppData/Contexts/File/Repositories/IFileRepository.cs
+++ UpBoard.Application.AppData/Contexts/File/Repositories/IFileRepository.cs
@@ -11,11 +11,28 @@ namespace UpBoard.Application.AppData.Contexts.File.Repositories
 {
     public interface IFileRepository
     {
-
+        /// <summary>
+        /// Поиск файла по Id
+        /// </summary>
+        /// <param name="id">Id файла</param>
+        /// <param name="cancellation">Токен отмены</param>
+        /// <returns></returns>
         Task<InfoFileResponse> FindById(Guid id, CancellationToken cancellation);
 
+        /// <summary>
+        /// Добавление файла
+        /// </summary>
+        /// <param name="model">ДТО файла</param>
+        /// <param name="cancellation">Токен отмены</param>
+        /// <returns></returns>
         Task<Guid> AddAsync(CreateFileRequest model, CancellationToken cancellation);
 
+        /// <summary>
+        /// Удаление файла
+        /// </summary>
+        /// <param name="model">ДТО удаления файла</param>
+        /// <param name="cancellation">Токен отмены</param>
+        /// <returns></returns>
         Task DeleteAsync(DeleteFileRequest model, CancellationToken cancellation);
     }
 }
